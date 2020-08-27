60c3975fcad45152126c410459496a87d35d526d
ariful-sagor <ariful.islam200069@gmail.com>
2020-08-27 21:21:46 +0600

    Updates

---
+++ precache-manifest.26a5c4ad37e26b966f8fadd6f734ff61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f93d217dd6c54e155940b3f0385a1e",
+    "url": "/learnera/index.html"
+  },
+  {
+    "revision": "6c230a577d86bf322adf",
+    "url": "/learnera/static/css/main.e58a281e.chunk.css"
+  },
+  {
+    "revision": "4366d14e9ff9cbc1baf9",
+    "url": "/learnera/static/js/2.693d400d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/learnera/static/js/2.693d400d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c230a577d86bf322adf",
+    "url": "/learnera/static/js/main.13a3ef11.chunk.js"
+  },
+  {
+    "revision": "dc816439730bc18b1249",
+    "url": "/learnera/static/js/runtime-main.07c7d374.js"
+  }
+]);
